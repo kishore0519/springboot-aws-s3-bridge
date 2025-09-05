32ceb52945a33f4ebc1fa11f7e138614345c3561
kishore0519 <kishorekoppolu1905@gmail.com>
2025-09-05 11:38:19 +0530

    added

---
+++ src/main/java/com/kj/SpringbootAwsS3BridgeApplication.java
@@ -0,0 +1,13 @@
+package com.kj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsS3BridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsS3BridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kj/SpringbootAwsS3BridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsS3BridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
